c3b9508718dbba6908732a21bb265ba78e2f6936
Zeee <brianyudhistira1@gmail.com>
2024-01-02 18:12:10 +0700

    add final game

---
+++ Finding Budi1/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,101 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void WaterReflection_Awake_m86F43948E686275B6F0C6D761C619EDAB40B1678 (void);
+extern void WaterReflection_Update_mEAF4BCA28F1154AF68D716AC6E3A954482DED822 (void);
+extern void WaterReflection_RenderReflection_m01B0AD1623A4577A51AD8B1F3AE875FA24A80BF6 (void);
+extern void WaterReflection_Validate_m502E830C820B21FB6C83692F56E4C43DCA5DF19E (void);
+extern void WaterReflection__ctor_m9E43D7FFE829D81B2D6B290985815779F495C758 (void);
+extern void CameraPlayer_Start_mE391D1AC69C915ABE6470144A780DA71AB61E95E (void);
+extern void CameraPlayer_LateUpdate_mA31F02B113D5D66B37D89322303759D988121684 (void);
+extern void CameraPlayer__ctor_m35E2A08BDC9B80BD06677095BF3B4795693130EF (void);
+extern void PlayerControl_Start_mFADBD79243A6B6984704BCCACC7320F96F4D34C7 (void);
+extern void PlayerControl_Update_m3A67C6EE4E8A895323A173E7D58C82ECC002C428 (void);
+extern void PlayerControl__ctor_m2B3A599A61BE9CCA98820EB2390E965CBE920915 (void);
+extern void PlayerControl_U3CUpdateU3Eg__RotateToTargetU7C18_0_m1D772D691C3CD6F954A4A0F0688C82FD5186F4CA (void);
+extern void U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed__ctor_mD42A39E90655965A964FE6355099B57166B4AE1D (void);
+extern void U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_IDisposable_Dispose_mB59331DD864E1050DB66BB06E4B363D32CE3F922 (void);
+extern void U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_MoveNext_m3F961DCC75391B045F564CCEB71FE1C1CB0FEC0A (void);
+extern void U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCF8ED21A19ADF09290324AEFE2FD29FAFE3B7040 (void);
+extern void U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_Collections_IEnumerator_Reset_m0C98F7DD6C1135744F0A32257F26F73492A0ABBF (void);
+extern void U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_Collections_IEnumerator_get_Current_mE9CD514138A5F82188349166128666CAA95D7387 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[20] = 
+{
+	WaterReflection_Awake_m86F43948E686275B6F0C6D761C619EDAB40B1678,
+	WaterReflection_Update_mEAF4BCA28F1154AF68D716AC6E3A954482DED822,
+	WaterReflection_RenderReflection_m01B0AD1623A4577A51AD8B1F3AE875FA24A80BF6,
+	WaterReflection_Validate_m502E830C820B21FB6C83692F56E4C43DCA5DF19E,
+	WaterReflection__ctor_m9E43D7FFE829D81B2D6B290985815779F495C758,
+	CameraPlayer_Start_mE391D1AC69C915ABE6470144A780DA71AB61E95E,
+	CameraPlayer_LateUpdate_mA31F02B113D5D66B37D89322303759D988121684,
+	CameraPlayer__ctor_m35E2A08BDC9B80BD06677095BF3B4795693130EF,
+	PlayerControl_Start_mFADBD79243A6B6984704BCCACC7320F96F4D34C7,
+	PlayerControl_Update_m3A67C6EE4E8A895323A173E7D58C82ECC002C428,
+	PlayerControl__ctor_m2B3A599A61BE9CCA98820EB2390E965CBE920915,
+	PlayerControl_U3CUpdateU3Eg__RotateToTargetU7C18_0_m1D772D691C3CD6F954A4A0F0688C82FD5186F4CA,
+	U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed__ctor_mD42A39E90655965A964FE6355099B57166B4AE1D,
+	U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_IDisposable_Dispose_mB59331DD864E1050DB66BB06E4B363D32CE3F922,
+	U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_MoveNext_m3F961DCC75391B045F564CCEB71FE1C1CB0FEC0A,
+	U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCF8ED21A19ADF09290324AEFE2FD29FAFE3B7040,
+	U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_Collections_IEnumerator_Reset_m0C98F7DD6C1135744F0A32257F26F73492A0ABBF,
+	U3CU3CUpdateU3Eg__RotateToTargetU7C18_0U3Ed_System_Collections_IEnumerator_get_Current_mE9CD514138A5F82188349166128666CAA95D7387,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[20] = 
+{
+	1111,
+	1111,
+	1111,
+	1111,
+	1111,
+	1111,
+	1111,
+	1111,
+	1111,
+	1111,
+	1111,
+	1083,
+	930,
+	1111,
+	1057,
+	1083,
+	1111,
+	1083,
+	2151,
+	1111,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	20,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
